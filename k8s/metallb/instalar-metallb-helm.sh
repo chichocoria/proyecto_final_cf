93f4dfc4837b5e828076e8a154cfe37c42af8340
Chicho Coria <rubencoria7@gmail.com>
2024-06-05 19:38:36 +0000

    agregando script metallb

--- k8s/metallb/instalar-metallb-helm.sh
+++ k8s/metallb/instalar-metallb-helm.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+#1 - Agregar MetalLB al respositorio de Helm
+helm repo add metallb https://metallb.github.io/metallb
+#2 - Hacer un update del repo
+helm repo update
+#3 - Crear el namespace
+kubectl create namespace metallb
+#4 - Install metalLB en el namespace metallb
+helm install metallb metallb/metallb --namespace metallb
