e8b8b47eaabfcd40f94f2099312f036f5f5cd986
Chicho Coria <rubencoria7@gmail.com>
2024-06-06 15:38:27 +0000

    add task to playbook install master node

--- k8s/metallb/instalar-metallb-helm.sh
+++ k8s/metallb/instalar-metallb-helm.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Verifica si Helm está instalado
+if ! command -v helm &> /dev/null
+then
+    echo "Helm no está instalado. Por favor, instala Helm primero."
+    exit 1
+fi
+
 #1 - Agregar MetalLB al respositorio de Helm
 helm repo add metallb https://metallb.github.io/metallb
 #2 - Hacer un update del repo
@@ -8,3 +15,11 @@ helm repo update
 kubectl create namespace metallb
 #4 - Install metalLB en el namespace metallb
 helm install metallb metallb/metallb --namespace metallb
+#5 - Verificar la instalación
+kubectl get pods -n metallb-system
+#6 Aplicar la configuración de MetalLB
+kubectl apply -f metallb-config.yaml
+# Verificar la configuración
+kubectl get configmap -n metallb-system
+
+echo "MetalLB ha sido instalado y configurado correctamente."
