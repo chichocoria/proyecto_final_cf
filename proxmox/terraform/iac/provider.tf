4eb3e70beef65737f571bd3a11ada1c0d2118d7d
Chicho Coria <rubencoria7@gmail.com>
2024-06-18 14:23:09 +0000

    borrando tfstate para usarlo con terraform cloud

--- proxmox/terraform/iac/provider.tf
+++ proxmox/terraform/iac/provider.tf
@@ -11,7 +11,7 @@ terraform {
       version = "3.0.1-rc2"
     }
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
       version = "~> 4.0"
     }
   }
